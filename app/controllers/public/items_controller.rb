e8e40bc0b61cf56ec9bd5fa1c372b80abb6df225
EC2 Default User <k.nulaw20.gm.9406@gmail.com>
2022-10-19 13:07:31 +0000

    Initial Commit

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -9,9 +9,6 @@ class Public::ItemsController < ApplicationController
     @cart_item = CartItem.new
   end
   
-    # 消費税を加えた商品価格
-  def add_tax_price
-      (self.price * 1.08).round
-  end
+
 
 end
