a51771b3f3d5deeb69fea8af914d9377c41c40c0
EC2 Default User <k.nulaw20.gm.9406@gmail.com>
2022-10-19 12:39:51 +0000

    2kaime

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -3,10 +3,15 @@ class Public::ItemsController < ApplicationController
     @item = Item.new
     @items = Item.all
   end
-  
+
   def show
     @item = Item.find(params[:id])
     @cart_item = CartItem.new
   end
   
+    # 消費税を加えた商品価格
+  def add_tax_price
+      (self.price * 1.08).round
+  end
+
 end
