17a396e6e99e77e5b8e3cefe1ea987fe9c554f5f
EC2 Default User <ec2-user@ip-172-31-2-223.ap-northeast-1.compute.internal>
2022-10-19 08:25:09 +0000

    [Add]root,orders_controller

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,2 +1,21 @@
 class Public::OrdersController < ApplicationController
+  
+  def new 
+    @orders = Orders.new
+  end
+  
+  def create
+    @order = Order.new(order_params)
+    @order.save
+    redirect_to 'orders/complete'
+  end
+  
+  def index
+    @orders = Order.all
+  end
+  
+  def show
+    @order = Order.find(params[:id])
+  end
+  
 end
