c56fecfa333caf83033601c817dfcf62baeb6aaf
EC2 Default User <ec2-user@ip-172-31-10-166.ap-northeast-1.compute.internal>
2022-10-20 13:05:13 +0000

    [Add]registrations view作成中

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
   before_action :customer_state, only: [:create]
