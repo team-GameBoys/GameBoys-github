6a22c8b1ec6c6e3e9b620a5471cee1dc3479e4ee
EC2 Default User <ec2-user@ip-172-31-2-223.ap-northeast-1.compute.internal>
2022-10-23 04:18:29 +0000

    order.controller confirm new

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -4,22 +4,31 @@ class Public::OrdersController < ApplicationController
 
   def confirm
     @order = Order.new
+    # new 画面から渡ってきたデータを @order に入れる
    if params[:order][:select_address] == "1"
+       # view で定義しているselect_address  を選択した場合
      @order.post_code = current_customer.post_code
      @order.address = current_customer.address
      @order.name = current_customer.first_name + current_customer.last_name
+       # current_customerの住所を保存する
    elsif params[:order][:select_address] == "2"
+       # view で定義しているselect_address 2 を選択した場合
      @deliveries = Deliveries.find(params[:order][:deliveries_id])
      @order.post_code = @deliveries.post_code
      @order.address = @deliveries.address
      @order.name = @deliveries.name
+       # deliveriesの配送先住所を保存する 
    elsif params[:order][:select_address] == "3"
-     @order.post_code = params([:order][:post_code]
+       # view で定義しているselect_address 3 を選択した場合
+     @order.post_code = params([:order][:post_code])
      @order.address = params([:order][:address])
      @order.name = params([:order][:name])
+     　# new/viewで作ったフォームから保存する
    end
   end
-
+  
+  
+  
   private
 
   def order_params
