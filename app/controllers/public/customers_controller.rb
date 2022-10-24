51945af7d27c6a63aa691584d6b47c65833f79c9
maru9 <111681519+klfn9@users.noreply.github.com>
2022-10-24 01:32:28 +0000

    maru

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -12,7 +12,7 @@ class Public::CustomersController < ApplicationController
 
   def update
     @customer = current_customer
-    @customer.update
+    @customer.update(customer_params)
     redirect_to my_page_path
   end
   
