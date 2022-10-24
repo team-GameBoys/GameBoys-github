9b9253ed069523572cbdaa7e08e7efe2bbc830b2
EC2 Default User <ec2-user@ip-172-31-10-166.ap-northeast-1.compute.internal>
2022-10-24 05:03:00 +0000

    [Fix]h2レイアウト及びcart_items/controller&viewを修正

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,15 +2,12 @@ class CartItem < ApplicationRecord
   belongs_to :customer
   belongs_to :item
 
-
-  # def sum_of_price
-  #   item.price*quantity
-  # end
-
   def subtotal
     item.add_tax_price*quantity
   end
-  
 
-  
+  def add_tax_price
+      (self.price * 1.1).round
+  end
+
 end
