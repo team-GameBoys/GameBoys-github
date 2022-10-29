cf956cd8d4e8e4bca38f1b6ba93503b527bb942d
EC2 Default User <ec2-user@ip-172-31-10-166.ap-northeast-1.compute.internal>
2022-10-29 04:13:14 +0000

    [Finish]kannsei

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,26 +1,16 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # def after_sign_in_path_for(resource_or_scope)
+  #   if resource_or_scope.is_a?(Admin)
+  #     admin_home_top_path
+  #   else
+  #     items_path
+  #   end
+  # end
 
-  def after_sign_up_path_for(resource)
-    my_page_path(current_customer.id)
-  end
-
-  def after_sign_in_path_for(resource_or_scope)
-  if resource_or_scope.is_a?(Admin)
-    admin_home_top_path
-  else
-    items_path
-  end
-  end
-
-  def after_sign_out_path_for(resource)
-    admin_session_path
-  end
-
-
-  protected
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :post_code, :address, :phone_number, :password, :password_confirmation])
-  end
+  # protected
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :post_code, :address, :phone_number, :password, :password_confirmation])
+  # end
 end
